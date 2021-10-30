e305f97729ebab9f7fbe81e96536e82ebd14529b
NereLenin <NereLenin25@yandex.ru>
2021-10-30 18:25:56 +0500

    First commit

---
+++ mvvm_test/ItemViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace mvvm_test
+{
+    class ItemViewModel
+    {
+        public ObservableCollection<ToDoItem> Items { set; get; }
+        public ItemViewModel()
+        {
+            Items = new ObservableCollection<ToDoItem>();
+            Items.Add(new ToDoItem("Пример"));
+        }
+
+        public string TextOfNewItem { set; get; }
+
+        public ICommand AddNewToDoCommand => new Command(AddItem);
+        public ICommand RemoveToDoCommand => new Command(RemoveItem);
+        void AddItem()
+        {
+            if (TextOfNewItem == "") return;
+
+            Items.Add(new ToDoItem(TextOfNewItem));    
+        }
+
+        void RemoveItem(object o)
+        {
+            Items.Remove(o as ToDoItem);
+        }
+
+    }
+}
---
+++ mvvm_test/ToDoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+    class ToDoItem
+    {
+        public string Text{ get; set;}
+
+        public ToDoItem(string Text)
+        {
+            this.Text = Text;
+        }
+    }
+
